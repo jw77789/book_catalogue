750ef35fe81dbe734603150c2dd9a9e11047ddba
jw77789 <JALEN.WHITE03@spsmail.cuny.edu>
2025-05-18 12:57:35 -0400

    Create models.py

---
+++ models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    author = db.Column(db.String(200))
+    page_count = db.Column(db.Integer)
+    average_rating = db.Column(db.Float)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
